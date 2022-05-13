e7e4b902a194a6334408d0c612c0b9c974df8c05
DurgeshGupt <durgeshgu106@gmail.com>
2022-05-14 02:13:28 +0530

    added welcome to employee wages

---
+++ src/Employeewagebuilder/Employeewagebuilder.java
@@ -0,0 +1,12 @@
+package Employeewagebuilder;
+
+public class Employeewagebuilder {
+
+    public static void main(String[] args) {
+
+        System.out.println("welcome to computation problem");
+
+
+
+    }
+}
