2cd4434e0fb9207c797f69d114c54b0c3271a671
DurgeshGupt <durgeshgu106@gmail.com>
2022-05-14 02:30:21 +0530

    Added  Employee status

--- src/Employeewagebuilder/Employeewagebuilder.java
+++ src/Employeewagebuilder/Employeewagebuilder.java
@@ -1,12 +1,20 @@
 package Employeewagebuilder;
 
 public class Employeewagebuilder {
+         static final int IS_FULL_TIME = 1;
 
     public static void main(String[] args) {
 
         System.out.println("welcome to computation problem");
+        double a = Math.floor(Math.random() * 10) % 2;
 
-
-
+        if (a == IS_FULL_TIME)
+        {
+            System.out.println("Employee is present");
+        }
+        else
+        {
+            System.out.println("Employee is absent");
+        }
     }
 }
